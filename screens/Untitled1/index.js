a7cfcacaf138ced0f6fe6c797eeca3d48def28e7
daniel palacios <daniel.palacios@crowdbotics.com>
2022-11-09 18:42:41 -0300

    Crowdbotics Studio: Update app screens

--- screens/Untitled1/index.js
+++ screens/Untitled1/index.js
@@ -3,7 +3,7 @@ import { View, StyleSheet } from "react-native";
 
 const Untitled1 = () => {
   return <View style={_styles.YUYxqkrw}>
-      </View>;
+      <View style={_styles.rjjoGGxm}></View><View style={_styles.SaRtnmge}></View></View>;
 };
 
 export default Untitled1;
@@ -14,5 +14,37 @@ const _styles = StyleSheet.create({
     padding: 10,
     position: "relative",
     height: "100%"
+  },
+  rjjoGGxm: {
+    left: 52,
+    top: 59,
+    position: "absolute",
+    height: 60,
+    width: 140,
+    backgroundColor: "#E4E4E4",
+    borderRadius: 0,
+    color: "#777777"
+  },
+  MhGihBBV: {
+    position: "unset"
+  },
+  QAGRRtFp: {
+    position: "unset"
+  },
+  SaRtnmge: {
+    left: 111,
+    top: 522,
+    position: "absolute",
+    height: 60,
+    width: 140,
+    backgroundColor: "#E4E4E4",
+    borderRadius: 0,
+    color: "#777777"
+  },
+  pgbmUlnN: {
+    position: "unset"
+  },
+  ROzoIuZf: {
+    position: "unset"
   }
 });
